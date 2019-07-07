a0c752116df2e07cc40ddb0f4a341e2a53d2bff3
GoldenFlash <892458829@qq.com>
2019-07-07 14:13:03 +0800

    .

--- routes/spider.js
+++ routes/spider.js
@@ -8,7 +8,7 @@ var fs = require('fs');
 var path = require("path")
 
 // var url = 'https://v.qq.com/x/cover/vbb35hm6m6da1wc/s00318jxyqt.html'
-var url = "https://www.jianshu.com/p/860371dde653"
+var url = "https://v.qq.com/x/cover/75ijb6qovzrhtp7.html"
 // 'https://cnodejs.org/'
 
 router.get("/",(req,res,next)=>{
@@ -20,12 +20,16 @@ router.get("/",(req,res,next)=>{
         if (err) {
             return next(err);
         }
-        var items = [];
+        let data = sres.text
+        let reg =/https(.*)?/g
+        
+        // var items = [];
         // sres.text 里面存储着网页的 html 内容，将它传给 cheerio.load，习惯性地命名为 `$`
         // 接下来就可以使用熟悉的 jquery 操作了
-        var $ = cheerio.load(sres.text);
-        var article = $('.data-note-content').html()
-        console.log()
+        // var $ = cheerio.load(sres.text);
+        // var article = $(".container_inner .player_container .mod_player .tenvideo_player").childNodes
+        // txp_player txp_player_desktop txp_autohide_progress
+        console.log(data.match(reg))
         // $('#topic_list .cell').each(function (index, element) {
         //     var $img = $(element).find('img');
         //     var $topic = $(element).find('.topic_title');
@@ -39,7 +43,7 @@ router.get("/",(req,res,next)=>{
         //     if (err) throw err;
         //     console.log('文件已被保存');
         // }); 
-        res.send(article);
+        res.send("article");
     });
     // res.send({
     //     message:"123"
